fcd4663ff428553dd35ce54c53016b7fbb3bd912
Wonho ha <35015234+Las-Wonho@users.noreply.github.com>
2018-05-28 16:13:50 +0900

    Array Added

--- Rust tour with Code/03_Type_is_exist.rs
+++ Rust tour with Code/03_Type_is_exist.rs
@@ -2,5 +2,6 @@ fn main(){
     let a : i16 = 10;
     let b : f32 = 1.23456;
     let s = "LAS";
-    print!("{}{}, {}",s,a, b);
+    let c: [i32; 5] = [1, 2, 3, 4, 5];
+    print!("{}, {}, {}, {}",s,c.len(),a, b);
 }
